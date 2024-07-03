ccc4a4dd7ae23e7a0ea610bd75d5bf92ca03a8c3
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-07-03 15:08:49 +0100

    fetched all pokemon data

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react"
 import CardList from "../components/CardList"
 
 const CardContainer = () => {
+
+    const [pokemonData, setPokemonData] = useState({})
+    const [pokemonCards, setPokemonCards] = useState([])
+
+    const fetchAllPokemon = async () => {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1500')
+        const data = await response.json()
+        setPokemonData(data) // may be an issue
+        setPokemonCards(data.results)
+    }
+
+    useEffect(() => {
+        fetchAllPokemon()
+    }, [])
 
     return(
         <CardList />
