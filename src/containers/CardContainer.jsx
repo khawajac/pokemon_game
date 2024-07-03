9d091fe59ae0d0f8b293d3ec16cc4ceb63eb0748
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-07-03 16:20:21 +0100

    modified data that was being fetched to obtain relevent pokemon data including details for each pokemon (images, stats etc.)

--- src/containers/CardContainer.jsx
+++ src/containers/CardContainer.jsx
@@ -3,19 +3,48 @@ import CardList from "../components/CardList"
 
 const CardContainer = () => {
 
-    const [pokemonData, setPokemonData] = useState({})
+    
     const [pokemonCards, setPokemonCards] = useState([])
+    const [loading, setLoading] = useState(false)
 
     const fetchAllPokemon = async () => {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1500')
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=15')
         const data = await response.json()
-        setPokemonData(data) // may be an issue
+        console.log(data);
         setPokemonCards(data.results)
     }
+
+    const fetchPokemonDetails = async (url) => {
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log("pokemon data", data);
+        return data
+    }
+
+    const fetchAllPokemonDetails = async () => {
+        
+        pokemonCards.forEach(async (pokemon, i) => {
+            console.log(pokemonCards);
+            const details = await fetchPokemonDetails(pokemon.url)
+            const cardsCopy = [...pokemonCards]
+            cardsCopy[i].details = details
+            setPokemonCards(cardsCopy)
+        })
+    }
+
 
     useEffect(() => {
         fetchAllPokemon()
     }, [])
+
+    useEffect(() => {
+        console.log(loading);
+        if (loading || pokemonCards.length <= 0) return;
+        console.log(pokemonCards); // 15 objs
+        console.log("setting loading to true");
+        setLoading(true)
+        fetchAllPokemonDetails()
+    }, [pokemonCards])
 
     return(
         <CardList pokemonCards={pokemonCards} />
