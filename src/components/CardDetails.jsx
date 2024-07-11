a5e267d218c7178917e5d789359c34e4aa2c3b35
Charisma <actuallycharisma@outlook.com>
2024-07-11 23:50:01 +0100

    updated the card details to show stats and base stats, correctly mapped over nested objects

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -5,19 +5,28 @@ const CardDetails = () => {
 
 
     const pokemonCards = useLoaderData(); 
+    console.log(pokemonCards.details.stats)
 
     return (
 
         pokemonCards.details ?
         <>
-            <section>
+            <section className="card-details">
                 <h2>{pokemonCards.details.name.toUpperCase()}</h2>
                 <img src={pokemonCards.details.sprites.other['showdown'].front_shiny} alt="{pokemonCards.details.name}" />
                 <ul>
                     <p>Abilities</p>
                     {pokemonCards.details.abilities.map((ability, index) => (
-                            <li key={index} id="abilities">{ability.ability.name}</li>
+                            <li key={index} id="abilities">{ability.ability && ability.ability.name ?
+                            ability.ability.name : 'Unknown'}</li>
                     ))}
+                </ul>
+                <ul>
+                    <p>Stats</p>
+                    {pokemonCards.details.stats.map((stat, index) => 
+                        <li key={index} id="stat-name">{stat.stat && stat.stat.name ? `${stat.stat.name}: ${stat.base_stat}` : 'Unknown'}</li>
+                
+                )}
                 </ul>
             </section>
         </>
