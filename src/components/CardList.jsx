486842bb697ce34d877f4f0878ec419e856bf9f7
Charisma <actuallycharisma@outlook.com>
2024-07-13 00:06:00 +0100

    added a search bar, filters in real time and state is correct when navigating to the library tab. however when you press refresh the cards disappear if the search bar is empty

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import PokemonCard from "./PokemonCard";
 const CardList = ({pokemonCards}) => {
+
+    const [searchCard, setSearchCard] = useState(""); 
+    const [filteredCards, setFilteredCards] = useState(pokemonCards); 
+
+    const handleSearchQuery = (e) => {
+        const pokemonName = e.target.value; 
+        setSearchCard(pokemonName); 
+
+        // filtering the pokemon cards based on the pokemon name being searched
+        if(pokemonName != ""){
+        e.preventDefault(); 
+        const filteredPokemonCards = pokemonCards.filter(card => card.name.toLowerCase().includes(pokemonName.toLowerCase()))
+        setFilteredCards(filteredPokemonCards)
+        } else {
+            setFilteredCards(pokemonCards); 
+        }
+    }
     
-    const mappedPokemonCards = pokemonCards.map((pokemonCard, index) => {
+    const mappedPokemonCards = filteredCards.map((pokemonCard, index) => {
         return <PokemonCard 
         pokemonCard={pokemonCard}
         key={index}/>
@@ -10,6 +28,14 @@ const CardList = ({pokemonCards}) => {
     return (
         <>
             <h2>Card Library</h2>
+            <form id="search-card-list">
+                <input id="search-input" 
+                type="text" 
+                placeholder="Search Pokemon Name" 
+                value={searchCard}
+                onChange={handleSearchQuery}
+                />
+            </form>
             <section>
                 {mappedPokemonCards}
             </section>
